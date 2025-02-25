f371a791c9c504ad678de835089194d7c07f3c06
max18 <max18071998@gmail.com>
2025-02-25 15:03:50 +0300

    Сообщение о неверном логине или пароле

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,77 +1,82 @@
 import React from 'react';
-import {Formik, Form, Field, ErrorMessage} from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import {connect} from "react-redux";
-import {Navigate} from 'react-router-dom';
-import {login} from "../../redux/auth-reducer";
-import s from "./login.module.css"
-
-const validateLoginForm = values => {
-    const errors = {};
-    if (!values.email) {
-        errors.email = 'Введите email';
-    } else if (
-        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-    ) {
-        errors.email = 'Не правильный email';
-    }
-    return errors;
-};
+import { connect } from "react-redux";
+import { Navigate } from 'react-router-dom';
+import { login } from "../../redux/auth-reducer";
+import s from "./login.module.css";
 
 const validationSchemaLoginForm = Yup.object().shape({
-
+    email: Yup.string()
+        .email("Неправильный email")
+        .required("Введите email"),
     password: Yup.string()
-        .min(2, "Минимальное колличество символов 2")
-        .max(20, "Максимальное колличество символов 20")
-        .required("Введите пароль")
+        .min(2, "Минимальное количество символов 2")
+        .max(20, "Максимальное количество символов 20")
+        .required("Введите пароль"),
 });
 
-const Login = (props) => {
-
-    if (props.isAuth) return <Navigate to='/profile'/>
+export function Login(props) {
+    if (props.isAuth) {
+        return <Navigate to="/profile" />;
+    }
 
     return (
         <div className={s.login}>
-            <h1>Вход ВКонтакте</h1>
+            <h1>Войти ВКонтакте</h1>
             <Formik
-                initialValues={{email: '', password: '', rememberMe: false, messages: null}}
-                validate={validateLoginForm}
+                initialValues={{
+                    email: '',
+                    password: '',
+                    rememberMe: false,
+                }}
                 validationSchema={validationSchemaLoginForm}
-
-                onSubmit={(values) => {
+                onSubmit={(values, { setSubmitting }) => {
                     props.login(values.email, values.password, values.rememberMe);
+                    setSubmitting(false);
                 }}
             >
-                {() => (
+                {({ isSubmitting }) => (
                     <Form>
                         <div>
-                            <Field type='email' name='email' placeholder='e-mail'/>
-                            <ErrorMessage name='email' component='p'/>
+                            <label htmlFor="email">Почта</label>
+                            <Field id="email" name="email" placeholder="Email" />
+                            <ErrorMessage name="email" component="div" className={s.errors} />
                         </div>
+
                         <div>
-                            <Field type='password' name='password' placeholder='password'/>
-                            <ErrorMessage name='password' component='p'/>
+                            <label htmlFor="password">Пароль</label>
+                            <Field id="password" name="password" placeholder="Password" type="password" />
+                            <ErrorMessage name="password" component="div" className={s.errors} />
                         </div>
+
                         <div>
-                            <Field type='checkbox' name='rememberMe'/>
-                            <label htmlFor='rememberMe'>Запомнить меня</label>
+                            <label>
+                                <Field id="rememberMe" name="rememberMe" type="checkbox" />
+                                Запомнить меня
+                            </label>
                         </div>
-                        <button type='submit'>Войти</button>
-                        <p>{props.messageError}</p>
+
+                        <button type="submit" disabled={isSubmitting}>Войти</button>
                     </Form>
                 )}
             </Formik>
+            {props.errorAuth && <div className={s.errors}>{props.errorAuth}</div>}
         </div>
     );
-};
-
-
-let mapStateToProps = (state) => {
-    return {
-        messageError: state.auth.messageError,
-        isAuth: state.auth.isAuth
-    }
 }
 
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth,
+    errorAuth: state.auth.errorAuth,
+});
 
-export default connect(mapStateToProps, {login})(Login);
+export default connect(mapStateToProps, { login })(Login);
+
+
+
+
+
+
+
+
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,50 +1,80 @@
-import {authAPI} from "../api/api";
+import { authAPI } from '../api/api';
+const SET_AUTH_USER_DATA = 'SET_AUTH_USER_DATA';
+const SET_ERROR = 'SET_ERROR';
 
-const SetUserData='setUserData';
-
-let initialState ={
-    userId: null,
+let initialState = {
+    id: null,
     email: null,
     login: null,
     isAuth: false,
-    isFetching: false,
+    errorAuth: null,
 };
+
 const authReducer = (state = initialState, action) => {
     switch (action.type) {
-        case SetUserData:
+        case SET_AUTH_USER_DATA:
             return {
                 ...state,
                 ...action.payload,
-
             };
-            default:
+        case SET_ERROR:
+            return {
+                ...state,
+                errorAuth: action.payload,
+            };
+
+        default:
             return state;
     }
 };
-export const setAuthUserData=(userId,email,login,isAuth) =>({type:SetUserData,payload:{userId,email,login,isAuth}});
-export const getAuthUserData=()=>(dispatch)=>{
-    authAPI.me()
-        .then(response => {
-            if(response.data.resultCode ===0){
-                let{id,login,email}=response.data.data;
-                dispatch(setAuthUserData(id,email,login,true));
+
+export const setError = (errorMessage) => {
+    return {
+        type: SET_ERROR,
+        payload: errorMessage,
+    };
+};
+
+export const setAuthUserData = (id, email, login, isAuth) => ({
+    type: SET_AUTH_USER_DATA,
+    payload: { id, email, login, isAuth },
+});
+
+export const getAuthUserData = () => {
+    return (dispatch) => {
+        authAPI.me().then((response) => {
+            if (response.data.resultCode === 0) {
+                let { id, email, login } = response.data.data;
+                dispatch(setAuthUserData(id, email, login, true));
             }
         });
-}
-export const login=(email,password,rememberMe)=>(dispatch)=>{
-    authAPI.login(email,password,rememberMe)
-        .then(response => {
-            if(response.data.resultCode ===0){
-                dispatch(getAuthUserData())
+    };
+};
+
+export const login = (email, password, rememberMe) => {
+    return (dispatch) => {
+        authAPI.login(email, password, rememberMe).then((response) => {
+            if (response.data.resultCode === 0) {
+                dispatch(getAuthUserData());
+                dispatch(setError(null));
+            } else {
+                const errorMessage = response.data.messages.length
+                    ? response.data.messages[0]
+                    : 'Some error occurred';
+
+                dispatch(setError(errorMessage));
             }
         });
-}
-export const logout=()=>(dispatch)=>{
-    authAPI.logout()
-        .then(response => {
-            if(response.data.resultCode ===0){
-                dispatch(setAuthUserData(null,null,null,false));
+    };
+};
+
+export const logout = () => {
+    return (dispatch) => {
+        authAPI.logout().then((response) => {
+            if (response.data.resultCode === 0) {
+                dispatch(setAuthUserData(null, null, null, false));
             }
         });
-}
+    };
+};
 export default authReducer;
